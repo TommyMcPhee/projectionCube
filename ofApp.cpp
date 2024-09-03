4c42637e18c3769b33a97c8f9aacc6458fca0060
Tommy McPhee <135555457+TommyMcPhee@users.noreply.github.com>
2024-09-03 10:14:39 -0700

    Add files via upload

--- ofApp.cpp
+++ ofApp.cpp
@@ -12,69 +12,106 @@ void ofApp::setup() {
 	envelopes[5] = { 1.0, twoThirds, oneThird, 0.0, 1.0 };
 	envelopes[6] = { 1.0, 0.5, 0.0, 0.5, 1.0 };
 	envelopes[7] = { 1.0, 0.0, oneThird, twoThirds, 1.0 };
+	audioSetup();
+	videoSetup();
 }
 
-//--------------------------------------------------------------
-void ofApp::update(){
-
+void ofApp::audioSetup() {
+	streamSettings.setOutListener(this);
+	streamSettings.setApi(ofSoundDevice::Api::MS_WASAPI);
+	streamSettings.sampleRate = sampleRate;
+	streamSettings.bufferSize = bufferSize;
+	streamSettings.numOutputChannels = channels;
+	ofSoundStreamSetup(streamSettings);
 }
 
-//--------------------------------------------------------------
-void ofApp::draw(){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::videoSetup() {
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::ofSoundStreamSetup(ofSoundStreamSettings &settings) {
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
+void ofApp::audioOut(ofSoundBuffer &buffer) {
+	for (int a = 0; a < buffer.getNumFrames(); a++) {
+		for (int b = 0; b < channels; b++) {
+			buffer[a * channels + b] = sample[b];
+		}
+	}
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::draw() {
+	refresh();
+	frameBuffer.begin();
+	shader.begin();
+	setUniforms();
+	frameBuffer.draw(0, 0);
+	shader.end();
+	frameBuffer.end();
+	frameBuffer.draw(0.0, 0.0);
+}
 
+void ofApp::refresh() {
+	width = (float)ofGetWidth();
+	height = (float)ofGetHeight();
+	frameBuffer.allocate(width, height);
+	ofClear(0, 0, 0, 255);
+	window.set(width, height);
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
--- ofApp.h
+++ ofApp.h
@@ -2,24 +2,35 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+	void setup();
+	ofSoundStreamSettings streamSettings;
+	static const int sampleRate = 48000;
+	static const int bufferSize = 256;
+	static const int channels = 2;
+	void audioSetup();
+	void videoSetup();
+	void ofSoundStreamSetup(ofSoundStreamSettings &settings);
+	array<float, 2> sample;
+	void audioOut(ofSoundBuffer &buffer);
+	ofFbo frameBuffer;
+	ofShader shader;
+	void refresh();
+	void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		array<int, 8> row;
-		array<array<float, 5>, 8> envelopes;
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+	array<int, 8> row;
+	array<array<float, 5>, 8> envelopes;
 };
